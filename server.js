da60ca84f02df942c543ef3af90dd0baee13adaf
Rituraj Tripsthy <rituraj.tripathy@gmail.com>
2023-12-09 21:38:23 +0530

    minor

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/swap", async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error(error.response.data.description);
-    res.status(500).send(`Error : ${error.response.data.description}`);
+    res.send(`Error : ${error.response.data.description}`);
   }
 });
 
