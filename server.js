47e58b039d3f654c40bd69f1285c9e0a8e7bbd26
Rituraj Tripsthy <rituraj.tripathy@gmail.com>
2023-12-09 19:17:43 +0530

    more params

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = 3000;
 
 app.get("/swap", async (req, res) => {
   const accountAddress = req.query.accountAddress;
+  const srcCoinAddr = req.query.srcCoinAddr;
+  const amt = req.query.amt;
   const url = "https://api.1inch.dev/swap/v5.2/56/swap";
   console.log(req.query);
 
@@ -13,9 +15,9 @@ app.get("/swap", async (req, res) => {
       Authorization: "Bearer 4WyjmE36P9R1UQDLFrszynHOkM4d15mW",
     },
     params: {
-      src: "0xeca88125a5adbe82614ffc12d0db554e2e2867c8",
+      src: srcCoinAddr,
       dst: "0x0d8ce2a99bb6e3b7db580ed848240e4a0f9ae153",
-      amount: "1",
+      amount: amt,
       from: accountAddress,
       slippage: "1",
     },
@@ -27,7 +29,7 @@ app.get("/swap", async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error(error.response.data.description);
-    res.status(500).send("An error occurred");
+    res.status(500).send(`Error : ${error.response.data.description}`);
   }
 });
 
